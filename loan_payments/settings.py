09ba4e6c834fa15b642da4805a4ec7ee27b97ff2
Devendra Prasad <devendraprasad1984@gmail.com>
2021-08-29 11:30:52 +0530

    updates

--- loan_payments/settings.py
+++ loan_payments/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q2dhk4z_d8%sly2!d_ei5_665lnf)_bolw!=u10#xpyms0clgt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
