8195791448a7d0a9c2a83c05a15671c0cbc7894c
Devendra Prasad <devendraprasad1984@gmail.com>
2021-08-29 13:47:51 +0530

    updates

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -17,6 +17,17 @@ class ModelTests(TestCase):
 
     def test_new_user_email_normalize(self):
         """test the email for new user"""
-        email = 'devendraprasad1984@gmail.com'
+        email = 'devendraprasad1984@GMAIL.com'
         user = get_user_model().objects.create_user(email, 'test123')
         self.assertEqual(user.email, email.lower())
+
+    def test_email_new_user_invalid(self):
+        """invalid or no email check"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_super_user(self):
+        """test creating a new super user"""
+        user=get_user_model().objects.create_superuser('test_super_user@gmail.com','test123')
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
