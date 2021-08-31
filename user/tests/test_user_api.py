ef17139fc3d37f0b7fcbe1f436bffea4b462277c
Devendra Prasad <devendraprasad1984@gmail.com>
2021-08-31 11:11:11 +0530

    updates

--- user/tests/test_user_api.py
+++ user/tests/test_user_api.py
@@ -1,22 +1,25 @@
-from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.test import TestCase
 from django.urls import reverse
-
-from rest_framework.test import APIClient
 from rest_framework import status
+from rest_framework.test import APIClient
 
 
 CREATE_USER_URL = reverse('user:create')
 DUMMY_USER = 'devendraprasad1984@gmail.com'
 
+
 def create_user(**params):
     return get_user_model().objects.create_user(**params)
+
 
 class PublicUserApiTests(TestCase):
     """Test the users API (public)"""
 
+
     def setUp(self):
-        self.client = APIClient
+        self.client = APIClient()
+
 
     def test_create_valid_user_success(self):
         """test creating user with valid payload"""
@@ -32,6 +35,7 @@ class PublicUserApiTests(TestCase):
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
 
+
     def test_user_exists(self):
         """check if user already exists"""
         payload = {
@@ -40,8 +44,9 @@ class PublicUserApiTests(TestCase):
         }
         create_user(**payload)
 
-        res = self.client.post(CREATE_USER_URL, payload)
+        res = self.client.post(CREATE_USER_URL, **payload)
         self.assertTrue(res.status_code, status.HTTP_400_BAD_REQUEST)
+
 
     def test_password_too_short(self):
         """test that password must be more than 5 characters long"""
@@ -49,7 +54,7 @@ class PublicUserApiTests(TestCase):
             'email': DUMMY_USER,
             'password': 'as2',
         }
-        res = self.client.post(CREATE_USER_URL, payload)
+        res = self.client.post(CREATE_USER_URL, **payload)
         self.assertTrue(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(email=payload['email']).exists()
         self.assertFalse(user_exists)
