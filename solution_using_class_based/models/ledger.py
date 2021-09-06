f1715bb9396bfa6b80e4dccf305f51becb4f475e
Devendra Prasad <devendraprasad1984@gmail.com>
2021-09-06 23:25:25 +0530

    updates

--- solution_using_class_based/models/ledger.py
+++ solution_using_class_based/models/ledger.py
@@ -3,37 +3,49 @@ from solution_using_class_based.utils import enums, utils
 
 class Ledger(enums.Enums):
     """store ledger entity details"""
-    id = None
-    name = None
-    uid = None
+    _loan_id = None
+    _uid = None
+    _customer_id = None
+    _bank_id = None
+    _emi_months = None
+    _emi_months_repaid = None
+    _loan_amount = None
+    _emi_amount = None
+    _rate = None
+    _period = None
+    _interest = None
+    _repaid_amount = None
+    _total_amount_pi = None
+    _active = None
+    _rate_of_interes = None
+    _no_of_years = None
+    _total_interest = None
 
 
-    def __init__(self, id, name):
+    def __init__(self, id):
         """initialize the fields"""
-        self.name = name
-        self.id = id
-        self.uid = utils.Utils(8).get_secret_key()
+        self._loan_id = id
+        self._uid = utils.Utils(8).get_secret_key()
 
 
     def __str__(self):
-        return f'ledger {self.name}, has id {self.id}'
+        return f'ledger -loan {self._loan_id} belongs to customer {self._customer_id} is taken from bank {self._bank_id}'
 
 
     def serialize(self):
         return {
-            self.key_id: self.id,
-            self.key_name: self.name,
-            self.key_uid: self.uid
+            self.key_loan: self._loan_id,
+            self.key_customer_id: self._customer_id,
+            self.key_bank_id: self._bank_id,
+            self.key_emi_months: self._emi_months,
+            self.key_emi_months_repaid: self._emi_months_repaid,
+            self.key_loan_amount: self._loan_amount,
+            self.key_emi_amount: self._emi_amount,
+            self.key_rate: self.rate_of_interest,
+            self.key_period: self._no_of_years,
+            self.key_interest: self._total_interest,
+            self.key_repaid_amount: self._repaid_amount,
+            self.key_total_amount_pi: self._total_amount_pi,
+            self.key_when: utils.Utils().get_timestamp(),
+            self.key_active: self._active,
         }
-
-
-    def get_id(self):
-        return self.id
-
-
-    def get_name(self):
-        return self.name
-
-
-    def get_uid(self):
-        return self.uid
